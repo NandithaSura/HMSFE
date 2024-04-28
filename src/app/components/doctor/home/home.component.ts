61e0f2a87ea96e917e3b1fd9ea45a9fa4790988f
Varma <mrvarma85@gmail.com>
2024-04-28 23:25:25 +0530

    FE with few admin routes

---
+++ src/app/components/doctor/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit{
+  show: boolean = false
+  ngOnInit(): void {
+      const role = localStorage.getItem('role')
+      if(role === 'admin'){
+        this.show = true
+      }
+  }
+
+}
